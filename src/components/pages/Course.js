dc3a401979d5451c2c651ffccd033de803bfb4e2
shamin ali <shamin.ali@quantiphi.com>
2022-06-19 16:36:14 +0530

    fixed course scroll up issue

--- src/components/pages/Course.js
+++ src/components/pages/Course.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useParams } from "react-router-dom"
 import CourseDetail from "../CourseDetail";
 import Footer from "../Footer";
+
 function Course() {
+
     let { id } = useParams();
+    useEffect(()=>{
+        window.scrollTo(0, 0)
+    },[])
+    
     return (
         <>
             <CourseDetail id={id} />
