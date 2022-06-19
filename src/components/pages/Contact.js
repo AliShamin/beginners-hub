6b59abecda37b20d92492dc0ba795013175ac4cc
shamin ali <shamin.ali@quantiphi.com>
2022-06-19 17:07:33 +0530

    fix contact scroll up issue

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Footer from "../Footer";
 import ContactInfo from '../ContactInfo';
 
 function Contact() {
+    useEffect(()=>{
+        window.scrollTo(0, 0)
+    },[])
+  
     return (<>
         <ContactInfo/>
         <Footer/>
