df855eb59ff4c9783b40d8ce4adffc2376609ab3
shamin ali <shamin.ali@quantiphi.com>
2022-06-23 12:54:58 +0530

    updated social media section as well as fix css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,10 @@ function Footer() {
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
             <h2>About Us</h2>
+            <Link to='/about'>About</Link>
             <Link to='/contact'>Contacts</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Terms of Service</Link>
+            <Link to='/'>Privacy Policy</Link>
+            <Link to='/'>Terms And Conditions</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
@@ -47,48 +47,6 @@ function Footer() {
             </Link>
           </div>
           <small class='website-rights'>BgHub © 2022</small>
-          <div class='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i class='fab fa-linkedin' />
-            </Link>
-          </div>
         </div>
       </section>
     </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,13 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
+import Profile from '../Profile';
 
 function Home() {
   return (
     <>
-    <HeroSection/>
+      <HeroSection />
+      <Profile />
       <Cards />
       <Footer />
     </>
