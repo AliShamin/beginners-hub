f3fd1f6e34723ae5c04df6dc91bec709472a0689
shamin ali <shamin.ali@quantiphi.com>
2022-06-24 23:12:54 +0530

    feat: updated services section

--- src/components/ServiceOffering.js
+++ src/components/ServiceOffering.js
@@ -6,24 +6,37 @@ function ServiceOfferings() {
     return (
         <>
             <section className="service-offerings-container">
-                <section className="service-offering-box">
-                    <div>
-                        <h1>WHAT WE OFFER ?</h1>
-                        {serviceOfferings.map((e) => (
-                            <div className="offering-description-box">
-                                <h2>{e['offerTitle']}</h2>
-                                <p>{e['offerDescription']}</p>
-                            </div>
-                        ))}
-                    </div>
-                    <div>
-                        <h1>OUR ONLINE SCHEDULE</h1>
-                        <div className="offering-description-box">
-                            <h3>Book our free Online Classes here</h3>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <div>
+                    <section className="service-offerings-nav-container">
+                        <div className="service-offerings-nav-box">
+                            {serviceOfferings.map((e) => (
+                                <div className="d-flex flex-row">
+                                    <i class={e.offeringIcon}></i>
+                                    <div>
+                                        {e['offerTitle']}
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                    </div>
-                </section>
+                    </section>
+                    <section className="service-offering-content-container">
+                        <div>
+                            {serviceOfferings.map((e) => (
+                                <div className="offering-description-box">
+                                    <h2>{e['offerTitle']}</h2>
+                                    <p>{e['offerDescription']}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <div>
+                            <h1>OUR ONLINE SCHEDULE</h1>
+                            <div className="offering-description-box">
+                                <h3>Book our free Online Classes here</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
             </section>
         </>
     );
