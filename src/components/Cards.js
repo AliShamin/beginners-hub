63ddd2afcf97f34aa971fb53fb4000442dd1014c
shamin ali <shamin.ali@quantiphi.com>
2022-07-21 01:23:35 +0530

    fix cards scroll logic

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../core/styles/Cards.css';
 import CardItem from './CardItem';
 import courseDetails from "../assets/CourseDetails.json";
 
 function Cards() {
 
-  const [startOfset, setStartOfset] = useState(0);
-  const [endOfset, setEndOfset] = useState(3);
+  let [startOfset, setStartOfset] = useState(0);
+  let [endOfset, setEndOfset] = useState(3);
+  const [showCardsScroll, setShowCardsScroll] = useState(false);
   const courseLength = courseDetails.length;
+
+  useEffect(() => {
+    if (courseDetails.length > 3) {
+      setShowCardsScroll(true)
+    }
+  },[])
 
   const moveCards = (direction) => {
 
@@ -15,33 +22,33 @@ function Cards() {
       if (endOfset == courseLength) {
         return;
       } else if (endOfset < courseLength) {
-        setStartOfset(endOfset);
-      }
-
-      if (endOfset + 3 <= courseLength) {
-        setEndOfset(endOfset + 3);
-      } else {
-        setEndOfset(courseLength);
+        setEndOfset(++endOfset);
+        setStartOfset(++startOfset);
       }
     }
     else if (direction === "backward") {
-      if (startOfset >= 3) {
-        setStartOfset(startOfset - 3);
-        setEndOfset(endOfset - 3);
+      if (startOfset == 0) {
+        return;
       }
+      setEndOfset(--endOfset);
+      setStartOfset(--startOfset);
     }
   }
 
   const getCardItems = (startOfset, endOfset) => {
     let cardItems = [];
+    console.log("startOfset",startOfset);
+    console.log("endofset",endOfset);
     for (let x = startOfset; x < endOfset; x++) {
-      cardItems.push(<CardItem
-        src={courseDetails[x].course_img_src}
-        text={courseDetails[x].courseTitle}
-        label={courseDetails[x].level}
-        path={courseDetails[x].path}
-        courseId={courseDetails[x].courseId}
-      />)
+      if (x < courseDetails.length) {
+        cardItems.push(<CardItem
+          src={courseDetails[x].course_img_src}
+          text={courseDetails[x].courseTitle}
+          label={courseDetails[x].level}
+          path={courseDetails[x].path}
+          courseId={courseDetails[x].courseId}
+        />)
+      }
     }
     return cardItems;
   }
@@ -50,17 +57,19 @@ function Cards() {
     <div className='cards'>
       <h1>Check out these courses!</h1>
       <div className='cards__container'>
-        <span onClick={() => { moveCards("backward") }}>
+        {showCardsScroll && <span onClick={() => { moveCards("backward") }}>
           <i className="fa fa-solid fa-backward" ></i>
         </span>
+        }
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             {getCardItems(startOfset, endOfset)}
           </ul>
         </div>
-        <span onClick={() => { moveCards("forward") }}>
+        {showCardsScroll && <span onClick={() => { moveCards("forward") }}>
           <i className="fa fa-solid fa-forward" ></i>
         </span>
+        }
       </div>
     </div>
   );
